3357d5086635ad90435d58ee95182b0fd9a52f4b
halothesleeper <artitamm8@gmail.com>
2023-06-15 15:19:38 +0200

    kinda final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useState } from 'react';
+import classes from './App.module.scss';
 
 function App() {
+  const [ weight, setWeight ] = useState(60);
+  const [ height, setHeight ] = useState(170);
+
+  const handleWeightChange = (e) => {
+    setWeight(e.target.value);
+  };
+
+  const handleHeightChange = (e) => {
+    setHeight(e.target.value);
+  };
+
+  const BMI = useMemo(() => {
+    const calculatedHeight = height / 100;
+    return (weight / (calculatedHeight * calculatedHeight)).toFixed(1);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={classes.wholeAppComponent}>
+      <section className={classes.calculatorSection}>
+        <header>
+          <h1>BMI Calculator</h1>
+        </header>
+        <div>
+          <p>Weight: {weight}kg</p>
+          <input
+            type='range'
+            step='1'
+            min='40'
+            max='220'
+            onChange={handleWeightChange}
+            value={weight}
+          />
+        </div>
+        <div>
+          <p>Height: {height}cm</p>
+          <input
+            type='range'
+            step='1'
+            min='140'
+            max='220'
+            onChange={handleHeightChange}
+            value={height}
+          />
+        </div>
+        <div>
+          <h2>Your BMI is:</h2>
+          <div className={classes.bmiSection}>
+            <p>{BMI}</p>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
